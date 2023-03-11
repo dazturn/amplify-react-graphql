6aca423dca7454c702cd8303e0dc801f67542bef
Daziah Turner <daziahturner1@gmail.com>
2023-03-11 09:50:34 -0800

    Fix for graphql API compile error.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqleeea868e": {
       "AppClientID": "string",
